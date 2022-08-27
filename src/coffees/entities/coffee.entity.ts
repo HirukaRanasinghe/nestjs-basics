c730b9d080aecdbf34d21579e9b00a8774c4ec8c
HirukaRanasinghe <62961312+HirukaRanasinghe@users.noreply.github.com>
2022-08-27 08:56:21 +0530

    Add Coffee Entity

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity() // sql table name will be coffee. always starts with simple case.
 export class Coffee{
+    @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
     name: string;
+
+    @Column()
     brand: string;
+
+    @Column('json', {nullable: true}) // Flavours should store arrays as JSON and it is optional 
     flavours: string[];
 }
